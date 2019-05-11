83f058312b3c4d9a403b92c8ba8f0dde1d6d9cc6
Francis Begyn <francis.begyn@gmail.com>
2019-05-11 09:27:52 +0200

    fix the webhook for basic Cisco stealthwatch

--- webhooks/stealthwatch/alerta_stealthwatch.py
+++ webhooks/stealthwatch/alerta_stealthwatch.py
@@ -7,14 +7,11 @@ class StealthwatchWebhook(WebhookBase):
     Good luck finiding any usefull documentation, it's cisco as usual
     """
     def incoming(self, query_string, payload):
-        #assigned_to = payload.get('assigned_to') or 'None'
-        #merit = payload.get('merit') or 'None'
-        #prio = payload.get('priority') or 'None'
+
         event = payload.get('type', 'No type found')
         resource = payload.get('hostname', 'No resource found')
-        #descr = payload.get('description') or 'None'
-        text = payload.get('text','No text found')
-        service = payload.get('source_name','No services found')
+        text = payload.get('text', 'No text found')
+        service = payload.get('source_name', 'No services found')
         resolved = payload.get('resolved', False)
 
         status = 'undefined'
@@ -27,13 +24,13 @@ class StealthwatchWebhook(WebhookBase):
             severity = 'warning'
 
         return Alert(
-                service=[service],
-                resource=resource,
-                event=event,
-                status=status,
-                severity=severity,
-                environment='Production',
-                text=text,
-                origin='Cisco Stealthwatch',
-                raw_data=payload
-            )
+            service=[service],
+            resource=resource,
+            event=event,
+            status=status,
+            severity=severity,
+            environment='Production',
+            text=text,
+            origin='Cisco Stealthwatch',
+            raw_data=payload
+        )
